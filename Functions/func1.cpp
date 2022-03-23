69530c5f3d14279841dd4c6587b5a261ca78f092
Sougato Roy <sougatoroy007@gmail.com>
2022-03-23 23:04:34 +0530

    Functions

--- Functions/func1.cpp
+++ Functions/func1.cpp
@@ -1,3 +1,34 @@
 #include<iostream>
 using namespace std;
 
+// signature of the function
+// body of the function
+// returnType functionName(parameter list){body of the function}
+
+int maximum(int a, int b); // forward declaration cause the function is after the main function
+
+void printMessage() // void function which returns nothing
+{
+    cout<<"Hello Bruhhhh!";
+}
+
+int main()
+{
+    int a,b;
+    cout<<"Enter two numbers to find the greatest one-"<<endl;
+    cin>>a>>b;
+
+    int c=maximum(a,b); // calling a function
+    cout<<c;
+    printMessage();
+    return 0;
+}
+
+int maximum(int x, int y)
+{
+    if(x>y)
+    return x;
+    else
+    return y;
+}
+
